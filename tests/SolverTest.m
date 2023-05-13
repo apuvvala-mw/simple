2bc8a6657acb8fbd98bbdee443c6f6aefccf0d83
apuvvala <apuvvala@host.docker.internal>
2023-05-13 14:18:48 -0400

    dummy change

--- tests/SolverTest.m
+++ tests/SolverTest.m
@@ -12,11 +12,13 @@ classdef SolverTest < matlab.unittest.TestCase
             expSolution = [2 1];
             testCase.verifyEqual(actSolution, expSolution)
         end
+
         function imaginarySolution(testCase)
             actSolution = quadraticSolver(1,2,10);
             expSolution = [-1+3i -1-3i];
             testCase.verifyEqual(actSolution,expSolution)
         end
+        
         function nonnumericInput(testCase)
             testCase.verifyError(@()quadraticSolver(1,"-3",2), ...
                 "quadraticSolver:InputMustBeNumeric")
